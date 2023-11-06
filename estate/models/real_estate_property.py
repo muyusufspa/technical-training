96b9f216808a9ec9fe6510a5a8b267c97f2e267d
muyusufspa <muyusuf@spa.sa>
2023-11-06 14:42:01 +0100

    Update real_estate_property.py

--- estate/models/real_estate_property.py
+++ estate/models/real_estate_property.py
@@ -9,8 +9,8 @@ class RealEstateProperty(models.Model):
     postcode = fields.Char(string='Postcode')
     date_availability = fields.Date(string='Availability Date')
     expected_price = fields.Float(string='Expected Price')
-    # prevent copying of selling price
-    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
+    # prevent copying of selling price, calculated field expected_price + 10%
+    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False, compute='_compute_selling_price')
     bedrooms = fields.Integer(string='Bedrooms')
     living_area = fields.Integer(string='Living Area')
     facades = fields.Integer(string='Facades')
@@ -19,3 +19,9 @@ class RealEstateProperty(models.Model):
     garden_area = fields.Integer(string='Garden Area')
     garden_orientation = fields.Char(string='Garden Orientation')
     kitchen_type = fields.Char(string='Kitchen Type')
+    
+    
+    def _compute_selling_price(self):
+        for record in self:
+            record.selling_price = record.expected_price * 1.1
+            
