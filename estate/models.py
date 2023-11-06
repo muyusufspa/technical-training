f3bbf053fd75bdf963efdddf27123ec20f595cc4
muyusufspa <muyusuf@spa.sa>
2023-11-06 11:07:51 +0100

    Update to model for Odoo

--- estate/models.py
+++ estate/models.py
@@ -1,17 +1,19 @@
-from django.db import models
+from odoo import models, fields
 
 class RealEstateProperty(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    postcode = models.CharField(max_length=10)
-    date_availability = models.DateField()
-    expected_price = models.FloatField()
-    selling_price = models.FloatField()
-    bedrooms = models.IntegerField()
-    living_area = models.IntegerField()
-    facades = models.IntegerField()
-    garage = models.BooleanField()
-    garden = models.BooleanField()
-    garden_area = models.IntegerField()
-    garden_orientation = models.CharField(max_length=255)
-    kitchen_type = models.CharField(max_length=255)
+    _name = 'real.estate.property'
+
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
+    postcode = fields.Char(string='Postcode')
+    date_availability = fields.Date(string='Availability Date')
+    expected_price = fields.Float(string='Expected Price')
+    selling_price = fields.Float(string='Selling Price')
+    bedrooms = fields.Integer(string='Bedrooms')
+    living_area = fields.Integer(string='Living Area')
+    facades = fields.Integer(string='Facades')
+    garage = fields.Boolean(string='Garage')
+    garden = fields.Boolean(string='Garden')
+    garden_area = fields.Integer(string='Garden Area')
+    garden_orientation = fields.Char(string='Garden Orientation')
+    kitchen_type = fields.Char(string='Kitchen Type')
