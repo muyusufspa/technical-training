6a3955ba630d701846e204f3373b82bafc150720
muyusufspa <muyusuf@spa.sa>
2023-11-06 15:02:58 +0100

    Update real_estate_property.py

--- estate/models/real_estate_property.py
+++ estate/models/real_estate_property.py
@@ -7,11 +7,13 @@ class RealEstateProperty(models.Model):
     name = fields.Char(string='Name', required=True)
     description = fields.Text(string='Description')
     postcode = fields.Char(string='Postcode')
-    date_availability = fields.Date(string='Availability Date')
+    # Set the default date to 3 months from today
+    date_availability = fields.Date(string='Availability Date', default=lambda self: fields.Date.today() + timedelta(days=90))
     expected_price = fields.Float(string='Expected Price')
     # prevent copying of selling price, calculated field expected_price + 10%
     selling_price = fields.Float(string='Selling Price', readonly=True, copy=False, compute='_compute_selling_price')
-    bedrooms = fields.Integer(string='Bedrooms')
+    bedrooms = fields.Integer(string='Bedrooms', default=2)
+    last_seen = fields.Datetime("Last Seen", default=lambda self: fields.Datetime.now())
     living_area = fields.Integer(string='Living Area')
     facades = fields.Integer(string='Facades')
     garage = fields.Boolean(string='Garage')
