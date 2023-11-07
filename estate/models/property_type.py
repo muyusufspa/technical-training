4871c2db00dfa5e46c69cd4e6e275e966b56da91
muyusufspa <muyusuf@spa.sa>
2023-11-07 14:04:02 +0100

    Update property_type.py

--- estate/models/property_type.py
+++ estate/models/property_type.py
@@ -1,5 +1,7 @@
 # Odoo model class for propoerty type:
 
+from odoo import models, fields
+
 class PropertyType(models.Model):
     _name = 'estate.property.type'
     _description = 'Property Type'
