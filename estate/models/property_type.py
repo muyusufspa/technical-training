2ca05aaffe575154d942eb200809dbeda1734e85
muyusufspa <muyusuf@spa.sa>
2023-11-07 15:56:16 +0100

    changes made to property type

--- estate/models/property_type.py
+++ estate/models/property_type.py
@@ -7,5 +7,3 @@ class PropertyType(models.Model):
     _description = 'Property Type'
     
     name = fields.Char(string='Name', required=True)
-    description = fields.Text(string='Description')
-    active = fields.Boolean(string='Active', default=True)
