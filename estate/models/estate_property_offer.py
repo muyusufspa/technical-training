e889540947c51b8c65f44b5c8f40fb7bffada5f6
muyusufspa <muyusuf@spa.sa>
2023-11-07 15:30:35 +0100

    Update estate_property_offer.py

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -4,6 +4,9 @@
 from odoo import models, fields
 
 class EstatePropertyOffer(models.Model):
+    _name = 'estate.property.offer'
+    _description = 'Real Estate Property Offer'
+    
     price = fields.Float(string='Price')
     status = fields.Selection(['Accepted', 'Refused', 'Pending'], string='Status')
     partner_id = fields.Many2one('res.partner', string='Customer', domain=[('is_salesman', '=', False)], context={'default_is_salesman': False} )
