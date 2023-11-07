049b852a1aa35457aad881f0a199bd8f9adcca1b
muyusufspa <muyusuf@spa.sa>
2023-11-07 15:35:21 +0100

    Update estate_property_offer.py

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -8,6 +8,7 @@ class EstatePropertyOffer(models.Model):
     _description = 'Real Estate Property Offer'
     
     price = fields.Float(string='Price')
-    status = fields.Selection(['Accepted', 'Refused', 'Pending'], string='Status')
+    # selections accepted, rejected, pending
+    status = fields.Selection([('accepted', 'Accepted'), ('rejected', 'Rejected'), ('pending', 'Pending')], default='peding', string='Status')
     partner_id = fields.Many2one('res.partner', string='Customer', domain=[('is_salesman', '=', False)], context={'default_is_salesman': False} )
     property_id = fields.Many2one('real.estate.property', string='Property')
