aa04487af59f9465a2a5c55d9d25d35fa0a3dd50
muyusufspa <muyusuf@spa.sa>
2023-11-07 16:25:35 +0100

    Update __manifest__.py

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,8 +5,8 @@
     "depends": ["base"],  # dependencies
     "data": [
         'security/ir.model.access.csv',
+        'views/estate_property_type_view.xml',        
         'views/estate_property_views.xml',
-        'views/estate_property_type_view.xml',
         'menues/estate_property_menu.xml',
     ],
     "installable": True,
